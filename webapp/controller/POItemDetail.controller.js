4794eb9efc0eb990c7c0117925eba6ce77586fdb
TunaTroy <datnthe172790@fpt.edu.vn>
2025-11-13 20:12:39 +0700

    Fix Bug load_PO

--- webapp/controller/POItemDetail.controller.js
+++ webapp/controller/POItemDetail.controller.js
@@ -1,64 +1,62 @@
-sap.ui.define([
-  "sap/ui/core/mvc/Controller",
-  "sap/m/MessageToast",
-  "sap/ui/core/routing/History",
+sap.ui.define(
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/core/routing/History",
+  ],
+  function (Controller, MessageToast, History) {
+    "use strict";
 
-], function (Controller, MessageToast, History) {
-  "use strict";
-
-  return Controller.extend("demodashboard.controller.POItemDetail", {
-
-     formatter: {
-      dateFormat: function (sDate) {
-        if (!sDate) return "";
-        const oDate = new Date(sDate);
-        return oDate.toLocaleDateString("en-GB");
+    return Controller.extend("demodashboard.controller.POItemDetail", {
+      formatter: {
+        dateFormat: function (sDate) {
+          if (!sDate) return "";
+          const oDate = new Date(sDate);
+          return oDate.toLocaleDateString("en-GB");
+        },
       },
-     },
-     
-    onInit: function () {
-      const oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-      oRouter.getRoute("POItemDetail").attachPatternMatched(this._onObjectMatched, this);
-    },
 
-    _onObjectMatched: function (oEvent) {
-  const sEbeln = oEvent.getParameter("arguments").Ebeln;
-  const sEbelp = oEvent.getParameter("arguments").Ebelp;
-
-    const oModel = this.getOwnerComponent().getModel();
-   this.getView().setModel(oModel);
-
-  const sPath = `/ProcurementItemSet(Ebeln='${sEbeln}',Ebelp='${sEbelp}')`;
-  console.log(" Binding path:", sPath); // ✅ dùng sau khi đã khai báo
-
- 
-
-  this.getView().bindElement({
-    path: sPath,
-    
-    events: {
-      dataRequested: () => sap.ui.core.BusyIndicator.show(0),
-      dataReceived: (oEvt) => {
-        sap.ui.core.BusyIndicator.hide();
-        console.log("✅ Data loaded:", oEvt.getParameter("data"));
-      }
-    }
-  });
-},
-
-
-
-
-    onNavBack: function () {
-      const oHistory = History.getInstance();
-      const sPreviousHash = oHistory.getPreviousHash();
-
-      if (sPreviousHash !== undefined) {
-        window.history.go(-1);
-      } else {
+      onInit: function () {
         const oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-        oRouter.navTo("POList", {}, true);
-      }
-    }
-  });
-});
+        oRouter
+          .getRoute("POItemDetail")
+          .attachPatternMatched(this._onObjectMatched, this);
+      },
+
+      _onObjectMatched: function (oEvent) {
+        const sEbeln = oEvent.getParameter("arguments").Ebeln;
+        const sEbelp = oEvent.getParameter("arguments").Ebelp;
+
+        const oModel = this.getOwnerComponent().getModel();
+        this.getView().setModel(oModel);
+
+        const sPath = `/ProcurementItemSet(Ebeln='${sEbeln}',Ebelp='${sEbelp}')`;
+        console.log(" Binding path:", sPath); // ✅ dùng sau khi đã khai báo
+
+        this.getView().bindElement({
+          path: sPath,
+
+          events: {
+            dataRequested: () => sap.ui.core.BusyIndicator.show(0),
+            dataReceived: (oEvt) => {
+              sap.ui.core.BusyIndicator.hide();
+              console.log("✅ Data loaded:", oEvt.getParameter("data"));
+            },
+          },
+        });
+      },
+
+      onNavBack: function () {
+        const oHistory = History.getInstance();
+        const sPreviousHash = oHistory.getPreviousHash();
+
+        if (sPreviousHash !== undefined) {
+          window.history.go(-1);
+        } else {
+          const oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+          oRouter.navTo("POList", {}, true);
+        }
+      },
+    });
+  }
+);
